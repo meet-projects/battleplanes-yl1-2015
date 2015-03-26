c3a754ccc63b33b3aa328fcc0f2621508df1f459
Nabil Alami <nabil16@meet.mit.edu>
2015-03-26 18:44:52 +0200

    Finished Marker

--- Markers.py
+++ Markers.py
@@ -1,16 +1,27 @@
 import turtle
-turtle.register_shape("image.gif")
-class Markers(Turtle): 
-	def __init__(self,loc,colour,hit):
-		RawTurtle.__init__(self)
-		self.loc=loc
+from turtle import *
+
+turtle.penup()
+turtle.ht()
+screen = turtle.getscreen()
+screen.register_shape("marker.gif")
+screen.register_shape("blue.gif")
+class Marker(Turtle): 
+	def __init__(self,x,y,colour,hit):
+		cv = turtle.getscreen()
+		RawTurtle.__init__(self,cv)
+		self.x=x
+		self.y=y
 		self.colour=colour
 		self.hit=hit
-		self.shape("image.gif")
-Markers m = new Markers("A1", "red",true)
-	
+		self.pu()
+		self.goto(x,y)
 
 
+		if hit==True:  
+			self.shape("marker.gif")
+		else:
+			self.shape("blue.gif")
 
 
-
+#m = Marker(50,50,"red",True)

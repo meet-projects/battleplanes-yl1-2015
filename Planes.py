67a2391e46d56e665ac114ab78492465f8fde3eb
shaltiel16-meet <shaltiel16@meet.mit.edu>
2015-04-06 20:01:27 +0300

    finished code - added planes

--- Planes.py
+++ Planes.py
@@ -26,20 +26,48 @@ class Plane(Turtle):
 		self.goto(x,y)
 		self.loc = "";
 	def released(self,x,y):
-
 		self.x = x
 		self.y = y
 		col = int((x + 400) / 50)
 		row = int((y - 200) / 50)
-		print(col,row)
+		
+		curX = 0
+		curY = 0
 		if (self.size % 2 == 0 and self.tilt == "v"):
-			self.goto(-400 + col * 50 + 25, 200 + row * 50 - 50)
+			curX = -400 + col * 50 + 25
+			curY = 200 + row * 50 - 50
 		elif (self.size % 2 == 0 and self.tilt == "h"):
-			self.goto(-400 + col * 50, 200 + row * 50 - 25)
+			curX = -400 + col * 50
+			curY = 200 + row * 50 - 25
 		else:
-			self.goto(-400 + col * 50 + 25, 200 + row * 50 - 25)
-
-		self.loc = str(col) + "," + str(row)
+			curX = -400 + col * 50 + 25
+			curY = 200 + row * 50 - 25
+		self.goto(curX, curY)
+		self.x = curX
+		self.y = curY
+		self.ondrag(self.passfunc)
+		self.onrelease(self.passfunc)
+		self.onclick(self.clicked)
+		
+		if (self.tilt == "v"):
+			self.startX = curX - 25
+			self.endX = curX + 25
+			self.startY = curY + self.size * 25
+			self.endY = curY - self.size * 25
+		else:
+			self.startX = curX - self.size * 25
+			self.endX = curX + self.size * 25
+			self.startY = curY + 25
+			self.endY = curY - 25
+		if (self.startX >= -400 and self.endX <= 0 and self.startY <= 200 and self.endY >= -200):
+			self.loc = str(col) + "," + str(row)
+		else:
+			self.loc = ""
+		#print(str(self.startX) + "," + str(self.endX) + "     " + str(self.startY) + "," + str(self.endY))
+	def clicked(self,x,y):
+		self.ondrag(self.goto)
+		self.onrelease(self.released)
+		self.onclick(self.passfunc)
 	def passfunc(self,x,y):
 		pass
 

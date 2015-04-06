04efdd364f62b190e7a2605128345367d54915d9
shaltiel16-meet <shaltiel16@meet.mit.edu>
2015-04-06 20:02:11 +0300

    Update Markers.py

--- Markers.py
+++ Markers.py
@@ -7,7 +7,7 @@ screen = turtle.getscreen()
 screen.register_shape("marker.gif")
 screen.register_shape("blue.gif")
 class Marker(Turtle): 
-	def __init__(self,x,y,colour,hit):
+	def __init__(self,x,y,colour,hit,visible):
 		cv = turtle.getscreen()
 		RawTurtle.__init__(self,cv)
 		self.x=x
@@ -15,11 +15,12 @@ class Marker(Turtle):
 		self.colour=colour
 		self.hit=hit
 		self.pu()
-
+		if (visible == False):
+			self.ht()
 		if hit==True:  
 			self.shape("marker.gif")
 		else:
 			self.shape("blue.gif")
+		self.speed(0)
 		self.goto(x,y)
-
 #m = Marker(50,50,"red",True)

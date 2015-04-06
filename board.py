e45111114e6fde21ea807e713ed9104045b0c143
shaltiel16-meet <shaltiel16@meet.mit.edu>
2015-04-06 20:01:50 +0300

    finished code - added board

--- board.py
+++ board.py
@@ -21,5 +21,31 @@ class Board():
 			plane.ht()
 		for marker in self.markers:
 			marker.ht()
-
+	def MarkerExistsAtLoc(self,x,y):
+		for marker in self.markers:
+			if (marker.x == x and marker.y == y):
+				return True
+		return False
+	def PlaneExistsAtLoc(self,x,y):
+		for plane in self.planes:
+			if (plane.sunk == True):
+				continue
+			if (x > plane.startX and x < plane.endX and y < plane.startY and y > plane.endY):
+				return True
+		return False
+	def CheckIfPlaneSunk(self,x,y):
+		for plane in self.planes:
+			if (plane.sunk == True):
+				continue
+			if (x > plane.startX and x < plane.endX and y < plane.startY and y > plane.endY):
+				count = 0
+				for marker in self.markers:
+					curX = marker.x
+					curY = marker.y
+					if (curX > plane.startX and curX < plane.endX and curY < plane.startY and curY > plane.endY):
+						count += 1
+				if (count == plane.size):
+					plane.sunk = True
+					return plane
+				return None
 
